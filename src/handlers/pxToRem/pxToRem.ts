470b731f96fe0de4d89053e78afb804c201298b4
TymekGluch <78880779+TymekGluch@users.noreply.github.com>
2023-09-08 15:53:26 +0200

    feat: adding function pxToRem

---
+++ src/handlers/pxToRem/pxToRem.ts
@@ -0,0 +1,37 @@
+const pxToRem = (px: number | string): string => {
+    const baseFontSize = 16;
+
+    if (typeof px === 'number') {
+        const resultInRem = px / baseFontSize;
+
+        return `${resultInRem}rem`;
+    } else {
+        const arrayWithValues = (px as string).split(' ');
+        const result = arrayWithValues.reduce((accumulator, current) => {
+            if (current.includes('rem') || current.includes('em') || current.includes('%')) {
+                return `${accumulator} ${current}`;
+            }
+
+            if (current.includes('px')) {
+                const numberFromString = parseFloat(current.replace('px', ''));
+                const result = numberFromString / baseFontSize;
+
+                return `${accumulator} ${result}rem`;
+            } else {
+                const numberFromString = parseFloat(current);
+                const result = numberFromString / baseFontSize;
+
+                return `${accumulator} ${result}rem`;
+            }
+        }, '');
+
+        return result;
+    }
+}
+
+export { pxToRem };
+
+
+
+
+
